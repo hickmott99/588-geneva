fd5b0d720a01ada2b3c2f2af7873fca5b4afd9f3
ralphworthington <90980328+ralphworthington@users.noreply.github.com>
2023-04-02 20:36:58 -0400

    Quack changes to print list of IPs

--- quack/analyze.py
+++ quack/analyze.py
@@ -7,22 +7,29 @@ fileNames = os.listdir(folderName)
 data = []
 
 for fileName in fileNames:
-    file = open(folderName + fileName)
-    lines = file.readlines()
-    file.close()
-    data += [json.loads(line) for line in lines]
+    with open(folderName + fileName) as file:
+        lines = file.readlines()
+        data += [json.loads(line) for line in lines]
 
 ips = set()
 domains = set()
 domainIpDict = defaultdict(lambda: set())   # domain -> set of ips
+ipDomainDict = defaultdict(lambda: set())   # ip -> set of domains
 
 for i in data:
     if i["received_error"] and ("timeout" in i["received_error"] or "reset" in i["received_error"]):
         ips.add(i["server_ip"])
         domains.add(i["domain"])
         domainIpDict[i["domain"]].add(i["server_ip"])
+        ipDomainDict[i["server_ip"]].add(i["domain"])
+
+sortedDomains = sorted(domainIpDict, key=lambda k: len(domainIpDict[k]), reverse=True)
+sortedIps = sorted(ipDomainDict, key=lambda k: len(ipDomainDict[k]), reverse=True)
 
 print("Unique IPs =", len(ips))
 print("Unique domains =", len(domains))
-sortedDomains = sorted(domainIpDict, key=lambda k: len(domainIpDict[k]), reverse=True)
 print(sortedDomains[:20])
+
+with open('genevadata.txt', 'a') as outputFile:
+    for ip in sortedIps:
+        print(ip, ipDomainDict[ip].pop(), file=outputFile)
