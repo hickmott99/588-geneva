60268c7fe5da8b265d7c3de8ef5756cc4388b4a0
Cooper Hickmott <hickmott99@gmail.com>
2023-04-05 18:26:57 -0400

    smol change

--- geneva/geneva_data_analyze.py
+++ geneva/geneva_data_analyze.py
@@ -11,18 +11,17 @@ strategy_found_in_all = {}
 for ii, file in enumerate(os.listdir(OUTPUT_DIR)):
     if file.startswith("idx") and file[-3:]=="csv":
         file_path = OUTPUT_DIR + file
-        
+
         with open(file_path) as file:
             csv_reader = csv.reader(file, delimiter=',')
             for line in csv_reader:
                 if line[0][0] != "A" and len(line)>=4:
                     strat = line[1]
-                    # Check if we shold add this strategy 
-                    if ii == 0:
+                    if strat not in strategy_found_in_all:
                         strategy_found_in_all[strat] = [[float(line[0]), float(line[2]), (line[3])]]
                     else:
-                        if strat in strategy_found_in_all:
-                            strategy_found_in_all[strat].append([float(line[0]), float(line[2]), (line[3])])
+                        strategy_found_in_all[strat].append([float(line[0]), float(line[2]), (line[3])])
+
 
 
 sort = sorted(strategy_found_in_all.items(), key=lambda x:len(x[1]), reverse=True)
@@ -32,7 +31,6 @@ for ii, val in enumerate(sort):
     prod = val[1][0][0]*val[1][0][1]
 
     average = 0
-
     # for each IP, Website pair with this strategy
     for val2 in val[1]:
         prod = val2[0]*val2[1]
